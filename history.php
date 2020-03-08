5764dde91633559ba58ab2ed7542c5cca357c1b0
Dexton Anderson <contact@dextonanderson.com>
2020-03-08 15:02:23 -0500

    add wiki redir

--- history.php
+++ history.php
@@ -4,7 +4,7 @@
     <?php
       $request_components = parse_url($_SERVER['REQUEST_URI']);
       $wiki_page = $request_components[2];
-      echo `<meta http-equiv="refresh" content="0; url=https://github.com/Ruinscraft/mcatlas-wiki/wiki` . $wiki_page . `">`;
+      echo `<meta http-equiv="refresh" content="0; url=https://github.com/Ruinscraft/mcatlas-history/wiki` . $wiki_page . `">`;
     ?>
   </head>
 </html>
