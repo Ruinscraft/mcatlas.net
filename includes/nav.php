1d0166487df80212e666506a22bfeb4614e891b7
Dexton Anderson <contact@dextonanderson.com>
2020-04-02 14:38:35 -0500

    remove bans page from sitemap and nav

--- includes/nav.php
+++ includes/nav.php
@@ -10,7 +10,6 @@
     <a class="button" href="/map">MAP</a>
     <a class="button" href="/rules">RULES</a>
     <a class="button" href="https://github.com/Ruinscraft/mcatlas-wiki/wiki">WIKI</a>
-    <a class="button" href="/bans">BANS</a>
     <a class="button" href="https://discordapp.com/invite/srSSSgJ">DISCORD</a>
 </nav>
 
