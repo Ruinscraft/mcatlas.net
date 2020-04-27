d0d4319311e5049e23769648dde548799c729e8e
drewdorris <contact@dorrisd.com>
2020-04-26 20:07:07 -0400

    update rules

--- rules.php
+++ rules.php
@@ -23,40 +23,40 @@
 				</ol>
 				<h3>Builds</h3>
 				<ol id="rule-list">
-					<li class="rule-list-entry">Do not significantly alter or destroy the surface of the world. You can cut down a tree, but don't blow up a mountain.</li>
+					<li class="rule-list-entry">Do not significantly alter or destroy the surface of the world. You can smoothen a hill, but don't blow up Mount Everest.</li>
 					<li class="rule-list-entry">Do not build unrealistic structures (sky islands, floating Towns, underwater cities, etc).</li>
 					<li class="rule-list-entry">Do not build lavacasts, unless it is used for a smooth wall or something similarly controlled.</li>
-					<li class="rule-list-entry">Do not create large islands (more than 1x1 chunk) in oceans. You are not allowed to create multiple islands in one area.</li>
+					<li class="rule-list-entry">Do not create artificial islands. Realistic ocean builds, such as oil rigs or ships, are allowed.</li>
 					<li class="rule-list-entry">Do not block waterways. You may build bridges; at least a 2 block gap between the surface of the water and the bottom of the bridge must be left so boats may easily pass underneath.</li>
 					<li class="rule-list-entry">Do not build bridges more than 4 blocks wide across large oceans or bays. Typically, ice bridges with side walls or railroad bridges are a good idea.</li>
 				</ol>
 				<h3>Griefing and Theft</h3>
 				<ol id="rule-list">
 					<li class="rule-list-entry">Griefing is not allowed.</li>
-					<li class="rule-list-entry">Griefing of unclaimed railways, paths, portals, hubs, etc. is not allowed.</li>
+					<li class="rule-list-entry">Griefing of unclaimed railways, paths, grinders, portals, hubs, etc. is not allowed (this includes in the Nether). You may alter these builds if you can claim them.</li>
 					<li class="rule-list-entry">If a build is seemingly abandoned, has no claims to it, and is not near another claim, you may claim/loot/destroy it. If it looks like it's part of a nearby Town, don't touch it!</li>
 					<li class="rule-list-entry">Griefing a seemingly abandoned build in an ugly way (lava, fire, random blocks, etc.) is not allowed.</li>
 					<li class="rule-list-entry">Theft of items (from chests) is discouraged but allowed. Town or Nation bank theft is easily provable and will result in heavy punishment. Ensure you lock your chests to prevent item theft by placing a sign on them.</li>
 					<li class="rule-list-entry">Theft or killing of mobs within towns is not allowed.</li>
-					<li class="rule-list-entry">Theft of items (by killing a player not properly warned of PvP) is not allowed.</li>
+					<li class="rule-list-entry">Theft of items by killing a player not properly warned of PvP is not allowed.</li>
 				</ol>
 				<h3>Towny (Towns and Nations)</h3>
 				<ol id="rule-list">
 					<li class="rule-list-entry">Towns can be named anything, but we recommend naming them after real life places. If you go to Chicago, you expect to see Chicago, not EpicBurgerTown.</li>
-					<li class="rule-list-entry">Nations must be named after a formerly or currently existing nation, territory, geographical feature, or city in real life. You can add titles such as "Empire" to the name.</li>
+					<li class="rule-list-entry">Nations must be named after a formerly or currently existing nation, territory, major geographical feature, or large city in real life. You can add titles such as "Empire" to the name.</li>
 					<li class="rule-list-entry">Towns and Nation names shouldn't be so similar to another Town or Nation that someone could be confused between them. ("Britain" vs. "GreatBritain")</li>
-					<li class="rule-list-entry">Towns can be any distance away from a Nation capital. The Nation's name must be relevant to the Nation's capital's location.</li>
-					<li class="rule-list-entry">You may only have up to 2 alternate accounts join a town. An alternate account cannot be the mayor of a town.</li>
+					<li class="rule-list-entry">A Nation's name must be relevant to the Nation's capital's location.</li>
+					<li class="rule-list-entry">You may only have up to 2 alternate accounts in Towns (not including a main account). An alternate account cannot be the mayor of a town.</li>
 					<li class="rule-list-entry">Do not create a Town or claim land close to another Town just to block the other Town's growth. You are allowed to expand near/next to it if given permission by the original Town. Don't go out of your way to claim near their Town!</li>
-					<li class="rule-list-entry">Odd town claims, including large unfilled outlines or disconnected singular claims, are discouraged. Staff have the discretion to remove these claims if they are in a high demand area, are unused, or have a poor appearance on the map.</li>
+					<li class="rule-list-entry">Odd Town claims, including large unfilled outlines or disconnected singular claims, are discouraged. Staff have the discretion to remove these claims if they are in a high demand area, are unused, or have a poor appearance on the map.</li>
 				</ol>
 				<h3>PvP</h3>
 				<ol id="rule-list">
-					<li class="rule-list-entry">PvP is only allowed if PvP is enabled by the Town. If you can't PvP, don't use other methods (lava, making a new claim, etc.) to kill someone.</li>
+					<li class="rule-list-entry">If you're not in a PvP enabled area, don't use other methods (lava, making a new claim, etc.) to kill someone.</li>
 					<li class="rule-list-entry">Towns must give at least a 15 second warning to nearby users before enabling PvP.</li>
 					<li class="rule-list-entry">Killing or attacking anybody without a fair warning of the fight is not allowed. If a Town already has PvP enabled, you are an outlaw in a Town, or you are in the Nether, that is also a fair warning.</li>
-					<li class="rule-list-entry">Trapping a player so that they could never escape by themselves (portal traps, bed traps, etc.) is not allowed, even if a Town has PvP enabled.</li>
-					<li class="rule-list-entry">If a staff member is performing staff duties, don't attack them. Be nice.</li>
+					<li class="rule-list-entry">Do not trap a player so that they could never escape by themselves (portal traps, bed traps, etc.).</li>
+					<li class="rule-list-entry">If a staff member is performing staff duties, do not attack them.</li>
 				</ol>
 			</div>
 		</div>
