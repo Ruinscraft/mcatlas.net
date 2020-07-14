ea8afe59068d6d0c535dab166846dbc0b210a974
drewdorris <contact@dorrisd.com>
2020-07-14 16:01:19 -0400

    add combat log rule

--- rules.php
+++ rules.php
@@ -56,6 +56,7 @@
 					<li class="rule-list-entry">Towns must give at least a 15 second warning to nearby users before enabling PvP.</li>
 					<li class="rule-list-entry">Killing or attacking anybody without a fair warning of the fight is not allowed. If a Town already has PvP enabled, you are an outlaw in a Town, or you are in the Nether, that is also a fair warning.</li>
 					<li class="rule-list-entry">Do not trap a player so that they could never escape by themselves (portal traps, bed traps, etc.).</li>
+					<li class="rule-list-entry">Logging out in a potential PvP situation ("combat logging") is not allowed.</li>
 					<li class="rule-list-entry">If a staff member is performing staff duties, do not attack them.</li>
 				</ol>
 			</div>
